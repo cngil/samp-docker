e0cb6a7827004dd590e56b279b2836f8b90e1c19
Ahmet Cengil <ahmet@cengil.co>
2024-07-22 21:20:06 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# Get the current directory
-current_dir=$(pwd)
-echo "Current directory: $current_dir"
+# Get the directory of the script
+script_dir=$(dirname "$(realpath "$0")")
+echo "Script directory: $script_dir"
 
-# Change to the bin directory
-build_dir="$current_dir/build"
+# Change to the build directory
+build_dir="$script_dir/build"
 echo "Entering directory: $build_dir"
-cd "$build_dir" || exit
+cd "$build_dir" || { echo "Failed to change to directory: $build_dir"; exit 1; }
 
-# Loop through all directories in the bin directory
+# Loop through all directories in the build directory
 for folder in */; do
   # Remove the trailing slash from the folder name
   folder_name=$(basename "$folder")
 
   # Change into the directory
   echo "Entering directory: $build_dir/$folder_name"
-  cd "$build_dir/$folder_name" || continue
+  cd "$build_dir/$folder_name" || { echo "Failed to change to directory: $build_dir/$folder_name"; continue; }
 
   # Run the docker build command
   echo "Building Docker image for folder: $folder_name"
-  docker build -t "cngil/samp:$folder_name" .
+  if ! docker build -t "cngil/samp:$folder_name" .; then
+    echo "Failed to build Docker image for folder: $folder_name"
+  fi
 
-  # Go back to the bin directory
+  # Go back to the build directory
   echo "Returning to directory: $build_dir"
-  cd "$build_dir" || exit
+  cd "$build_dir" || { echo "Failed to return to directory: $build_dir"; exit 1; }
 done
 
 echo "Docker build process completed for all folders."
